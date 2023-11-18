18af13f6e75ccd536e02a9bdf0249f0a4d6d558c
NoemiCalzada <noemi.calzada@bootcamp-upgrade.com>
2023-11-18 08:59:42 +0100

    estructura_fronted

---
+++ src/api/routes/users.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { register, login, profile, getUser, getUserById, getUserByEmail, putUser, deleteUser } = require("../controllers/users.controller");
+const { isAuth } = require("../../middleware/auth");
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/profile", [isAuth], profile); 
+router.get("/:id", getUserById);
+router.get("/", getUser);
+router.get("/:email", getUserByEmail);
+router.put("/:id", putUser);
+router.delete("/:id", deleteUser)
+
+module.exports = router;
