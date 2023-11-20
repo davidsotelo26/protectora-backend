731df860d1fb432bf31dcdbc496a8b20a3a817c5
NoemiCalzada <noemi.calzada@bootcamp-upgrade.com>
2023-11-20 11:34:03 +0100

    cambios a Mongo

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,32 +1,23 @@
-const express = require('express');
-const app = express();
-const mysql = require('mysql');
-require('dotenv').config();
 
-app.use(express.json());
+const mongoose = require("mongoose");
 
-const connection = mysql.createConnection({
-    host: process.env.DBHOST,
-    user: process.env.DBUSER,
-    password: process.env.DBPASSWORD,
-    database: process.env.DATABASE,
-});
 
-connection.connect((error) => {
-    if (error) throw error;
-    console.log('Conectado a la base de datos ');
-    return;
-});
+const connectDb = async () => {
+    try {
+        const db = await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        const { name, host } = db.connection;
+        console.log(`Nombre de la BD ${name} host : ${host}`)
 
-connection.query('SELECT * FROM Protectora', (error, results) => {
-    if (error) throw error;
-    console.log(results);
-});
+    } catch (error) {
+        console.log(error)
+    }
+}
+module.exports = { connectDb }
 
-app.get('/', (req, res) => {
-    res.send('Hola mundo');
-});
 
-app.listen(3001, () => {
-    console.log('Servidor escuchando en puerto 3001');
-});
+
+
+
